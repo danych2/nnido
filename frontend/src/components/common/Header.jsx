1d29a7cdeff2a6f25faa48c326fc51101acd9e82
danych2 <dduato@gmail.com>
2024-01-03 22:35:15 +0100

    fix header useselector import

--- frontend/src/components/common/Header.jsx
+++ frontend/src/components/common/Header.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useSelector } from 'react';
-import { useDispatch } from 'react-redux';
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { login, logout } from '../../actions/auth';
 import Button from './Button';
 
