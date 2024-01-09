5c0c8eaa45184c1aa15ef75f67f3ddcef72118ac
danych2 <dduato@gmail.com>
2024-01-09 23:29:52 +0100

    configured wsgi settings for prod

--- nnido/wsgi.py
+++ nnido/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'superg.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nnido.settings.prod')
 
 application = get_wsgi_application()
