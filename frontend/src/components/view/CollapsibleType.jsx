527375bd1009ad200fa475f93af732bb15ca93b9
danych2 <dduato@gmail.com>
2024-02-18 15:43:02 +0100

    remove animation from collapsibletype

--- frontend/src/components/view/CollapsibleType.jsx
+++ frontend/src/components/view/CollapsibleType.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect, useReducer } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { FaRegEye, FaRegEyeSlash } from 'react-icons/fa';
 import { GoTriangleRight, GoTriangleDown } from 'react-icons/go';
@@ -9,58 +9,53 @@ import './CollapsibleType.css';
 const CollapsibleType = ({
   title, isNode, pos, id, hidden, defaultChange,
   visibilityChange, isExpanded, setExpandedIndex,
-}) => {
-  const contentHeight = useRef(0);
-
-  return (
-    <>
-      <div
-        className="collapsible_background"
-        style={{ gridRow: `${pos * 2 + 2} / ${pos * 2 + 4}` }}
-      />
-      <div
-        className={`flex justify-between items-center gap-1 collapsible_button ${isExpanded ? 'open' : 'closed'}`}
-        onClick={() => setExpandedIndex(isExpanded ? null : id)}
-        style={{ gridRow: `${pos * 2 + 2}` }}
-      >
-        <span className="shrink-0">
-          {isExpanded ? (
-            <GoTriangleDown />
-          ) : (
-            <GoTriangleRight />
-          )}
-        </span>
-        <span className="grow truncate">
-          {title}
-        </span>
-        <button type="button" className="small_button" onClick={(e) => { e.stopPropagation(); visibilityChange(id); }}>
-          { hidden ? (
-            <FaRegEyeSlash />
-          ) : (
-            <FaRegEye />
-          )}
-        </button>
+}) => (
+  <>
+    <div
+      className="collapsible_background"
+      style={{ gridRow: `${pos * 2 + 2} / ${pos * 2 + 4}` }}
+    />
+    <div
+      className={`flex justify-between items-center gap-1 collapsible_button ${isExpanded ? 'open' : 'closed'}`}
+      onClick={() => setExpandedIndex(isExpanded ? null : id)}
+      style={{ gridRow: `${pos * 2 + 2}` }}
+    >
+      <span className="shrink-0">
+        {isExpanded ? (
+          <GoTriangleDown />
+        ) : (
+          <GoTriangleRight />
+        )}
+      </span>
+      <span className="grow truncate">
+        {title}
+      </span>
+      <button type="button" className="small_button" onClick={(e) => { e.stopPropagation(); visibilityChange(id); }}>
+        { hidden ? (
+          <FaRegEyeSlash />
+        ) : (
+          <FaRegEye />
+        )}
+      </button>
+    </div>
+    <input
+      className="collapsible_radio"
+      type="radio"
+      name={isNode ? 'node' : 'link'}
+      value={id}
+      onChange={defaultChange}
+      style={{ gridRow: `${pos * 2 + 2}` }}
+    />
+    <div
+      className="overflow-hidden col-start-1"
+      style={{ gridRow: `${pos * 2 + 3}`, maxHeight: isExpanded ? 'auto' : '0px' }}
+    >
+      <div className="m-1 p-1">
+        <EditType element_class={isNode ? 'node' : 'link'} typeId={id} />
       </div>
-      <input
-        className="collapsible_radio"
-        type="radio"
-        name={isNode ? 'node' : 'link'}
-        value={id}
-        onChange={defaultChange}
-        style={{ gridRow: `${pos * 2 + 2}` }}
-      />
-      <div
-        ref={contentHeight}
-        className="overflow-hidden transition-all duration-500 col-start-1"
-        style={{ gridRow: `${pos * 2 + 3}`, maxHeight: isExpanded ? contentHeight.current.scrollHeight : '0px' }}
-      >
-        <div className="m-1 p-1">
-          <EditType element_class={isNode ? 'node' : 'link'} typeId={id} />
-        </div>
-      </div>
-    </>
-  );
-};
+    </div>
+  </>
+);
 
 CollapsibleType.propTypes = {
   title: PropTypes.string.isRequired,
